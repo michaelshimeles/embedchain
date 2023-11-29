bf49757cc75a256489dd1ab7d67cc517effe18d4
Michael Shimeles <michaelwasihun96@gmail.com>
2023-11-29 16:12:30 -0500

    Added instructions to home dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
+import { CodeBlock } from '@skeletonlabs/skeleton';
 
 const Dashboard = ({ }) => {
 
@@ -14,6 +15,35 @@ const Dashboard = ({ }) => {
             <h2 className="mt-3 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
                 How it works
             </h2>
+            <div className='mt-4'>
+                <div className='flex items-center gap-1'>
+                    <p>Get your API key from
+                    </p>
+                    <Link className='flex items-center gap-1 hover:underline' href={'/dashboard/settings'}>
+                        Settings
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-external-link"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" /><polyline points="15 3 21 3 21 9" /><line x1="10" x2="21" y1="14" y2="3" /></svg>
+                    </Link>
+                </div>
+                <br />
+                <p>Fetch your embeddings:</p>
+                <div className='flex flex-col mt-4 gap-3'>
+                    <code className='flex px-4 py-2 border rounded-full'>
+                        <p>
+                            const response = await fetch("https://embedchain.vercel.app/api/embed", {
+                                `headers: {
+                                "x-api-key": API_KEY
+                            }`
+                            })
+                        </p>
+                    </code>
+                    <code className='flex px-4 py-2 border rounded-full'>
+                        <p>
+                            curl -H "x-api-key: API_KEY" "https://embedchain.vercel.app/api/embed"
+                        </p>
+                    </code>
+                </div>
+            </div>
+
         </div>
     );
 }
